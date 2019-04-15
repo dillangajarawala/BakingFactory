06e247efd6ecf47903f2c92326415132a614e2bd
Dillan Gajarawala <dillan.gajarawala@gmail.com>
2019-04-15 12:57:58 -0400

    authentication added and abilities started

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,6 @@ class UsersController < ApplicationController
     private
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :role, :password, :password_confirmation, :band_id)
+      params.require(:user).permit(:username, :role, :password, :password_confirmation)
     end
 end
