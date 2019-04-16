5a3f3100bfd527259f909c498ab60a2c82bd7b53
Dillan Gajarawala <dillan.gajarawala@gmail.com>
2019-04-15 23:30:01 -0400

    all ability tests passed

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,51 @@ class Ability
     if user.role? :admin
       can :manage, :all
     elsif user.role? :customer
-      
+      can :read, Item
+      can :show, Order do |order|
+        order.customer_id == user.customer.id
+      end
+      can :show, Customer do |customer|
+        customer.user_id == user.id
+      end
+      can :update, Customer do |customer|
+        customer.user_id == user.id
+      end
+      can :show, User do |u|
+        u.id == user.id
+      end
+      can :update, User do |u|
+        u.id == user.id
+      end
+      can :index, Order do |order|
+        order.customer_id == user.customer.id
+      end
+      can :checkout, Order do |order|
+        order.customer_id == user.customer.id
+      end
+      can :create, Order
+      can :add_to_cart, Order do |order|
+        order.customer_id == user.customer.id
+      end
+      can :create, Address
+      can :show, Address do |address|
+        address.customer_id == user.customer.id
+      end
+      can :update, Address do |address|
+        address.customer_id == user.customer.id
+      end
+      can :index, Address
+    elsif user.role? :baker
+      can :read, Item
+      can :index, Order
+    elsif user.role? :shipper
+      can :read, Item
+      can :read, Order
+      can :show, Address
+    else
+      can :read, Item
+      can :create, Customer
+    end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
