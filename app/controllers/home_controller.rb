35cc4e2b3f1e386ec311faefa7d00d57f482e07f
Dillan Gajarawala <dillan.gajarawala@gmail.com>
2019-04-27 23:58:20 -0400

    baker dashboard created

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,26 @@ class HomeController < ApplicationController
   def home
     if logged_in? && current_user.role?(:shipper)
       @unshipped_orders = Order.not_shipped.paginate(:page => params[:page]).per_page(3)
+    elsif logged_in? && current_user.role?(:baker)
+      @breads = baking_list("bread")
+      @muffins = baking_list("muffins")
+      @pastries = baking_list("pastries")
     end
+  end
+
+  def baking_list(category)
+    order_items = OrderItem.unshipped
+    items = Hash.new
+    order_items.each do |oi|
+      if oi.item.category == category
+        if !items.keys.include?(oi.item)
+          items[oi.item] = oi.quantity
+        else
+          items[oi.item] += oi.quantity
+        end
+      end
+    end
+    items
   end
 
   def about
